0356a571f4fb860745fba6e6a6cbcc67dfea40d5
D-Mosqueda <mosqdan8@gmail.com>
2022-10-07 05:34:58 -0700

    Movement
    player movement and animation. Some adjustments need to be made for Y movement animations.

---
+++ 4400Project/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+    private Vector2 direction;
+    private Animator animator;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        KeyInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * MoveSpeed * Time.deltaTime);
+        SetAnimator(direction);
+    }
+
+    private void KeyInput() 
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+    }
+    
+    private void SetAnimator(Vector2 direction)
+    {
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+        print(animator.GetFloat("xDir"));
+    }
+
+}
